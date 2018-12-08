4d31ab4895d1aebeebd7ffa1e598c1daa8299d25
Sambhav Saggi <smsaggi@gmail.com>
2018-12-08 15:23:24 -0500

    Potentiometer stuff

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors.java
@@ -10,6 +10,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
@@ -17,11 +18,25 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Sensors {
+    private static int POT_MAX = 270; // Max range in degrees
+
+    private static double Vmin = 0.004; // Minimum voltage
+
+    private static double Vmax = 3.304; // Maximum voltage
     // TODO: Initialize sensors
     BNO055IMU gyro;
+    AnalogInput pot;
     // Put all sensor stuff in here.
     // TODO: Constructor + JavaDoc
-    Sensors(BNO055IMU gyro) {
+    Sensors(BNO055IMU gyro, AnalogInput pot) {
         this.gyro = gyro;
+        this.pot = pot;
+    }
+    Sensors (AnalogInput pot) {
+        this.pot = pot;
+    }
+    public double getPot() {
+        return (POT_MAX/(Vmax-Vmin))*(pot.getVoltage()-Vmin);
     }
 }
+
