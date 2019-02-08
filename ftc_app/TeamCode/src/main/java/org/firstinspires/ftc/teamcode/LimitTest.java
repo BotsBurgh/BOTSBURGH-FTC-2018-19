0133401589ee15d086c6e7186f154c1668394de2
Sambhav Saggi <smsag_000@localhost.localdomain>
2019-02-08 17:28:39 -0500

    Buttons + Gradle

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimitTest.java
@@ -0,0 +1,45 @@
+/*
+Copyright 2019 FIRST Tech Challenge Team 11792
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * TODO: javadoc
+ */
+@TeleOp(name = "Limit Test", group = "Linear Op Mode")
+
+public class LimitTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        Button lim1 = new Button(hardwareMap.get(DigitalChannel.class, "lim1"));
+        telemetry.addData("> ", "Press Start to run.");
+        telemetry.update();
+        waitForStart();
+        // TODO: Comments
+        while(opModeIsActive()) {
+            telemetry.addData("Button: ", lim1.isPressed());
+            telemetry.update();
+        }
+    }
+}
