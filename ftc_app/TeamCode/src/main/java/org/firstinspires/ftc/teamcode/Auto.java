e389f9dee2c0631cb7547aec22b49cabdd08d071
nitsl <niteshramanathan@gmail.com>
2019-02-17 14:27:46 -0500

    Update Auto.java

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -88,6 +88,9 @@ RF is Red Far    (Far from the red team's crater)
 // TODO: JavaDoc
 @Autonomous(name="Gyro Test")
 public class Auto extends LinearOpMode {
+    private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
+    private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
+    private static final String LABEL_SILVER_MINERAL = "Silver Mineral";
     /* Declare OpMode members. */
     DcMotor motorFL,motorFR,motorBL,motorBR;
     DcMotor elevator;
@@ -98,6 +101,11 @@ public class Auto extends LinearOpMode {
     DistanceSensor sensorDistance;
     ElapsedTime runtime  = new ElapsedTime();
     Servo holder;
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+
+    private static final String VUFORIA_KEY = "AcM0K6z/////AAABmeiIHPqExEm6uvdttqzvUM8yc5vG8YPI75H9AWdWhYDwS3uA8rxBOa8gofNaaTRkLfYpu0EcoykMACJ9vm2u9D0uBFlsxkOSGnjSGZOH7jjS2A+rm0WyOyZ7krIdfoNm+2yV+nPqoQwFApuUDVN7d/HDXq+iW1P+21ZG1ahvPeDr4zJqoHLf9AvNaUzDWssKFBshs6MXdHPH7TaNAHebpqOwVvwOriBRaM/2ffxi/676+DEGypvu5pRcTwmzkCiP3BEdFVpG8BH1jUEcZ+GQd0s59hhqKV2tJZIQwQgvzZISTGSLZHZ06Ag5tOA+m9zIW5M8UpkdWrFEO7mGBRZnMmW0Ztle8Lg+lEHd6t5lZwuS";
+
 
 
     static final double COUNTS_PER_MOTOR_REV = 1120;    // eg: TETRIX Motor Encoder
